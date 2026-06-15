173ba7f8ce1482f30d8755ac2ad6b07766f202b3
dEN5-tech <moyadres_1@mail.ru>
2026-06-15 19:34:15 +0300

    fix2

--- build.rs
+++ build.rs
@@ -1,5 +1,8 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    println!("cargo:rustc-link-search=native=c:\\projects\\animesphere\\bin\\mpv-sdk");
+    if std::env::var("CARGO_CFG_TARGET_OS").as_deref() == Ok("windows") {
+        println!("cargo:rustc-link-search=native=bin\\mpv-sdk");
+        println!("cargo:rerun-if-changed=bin\\mpv-sdk");
+    }
 
     tonic_build::configure()
         .compile_protos(&["proto/anime.proto"], &["proto"])?;
